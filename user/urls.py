6776ca39e4f217dc71117f036f62d1b8ecc0211b
Luan0AVila <avila.almeida.luan@gmail.com>
2026-01-28 12:46:29 -0300

    criando o register_create

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 app_name = 'user'
 
 urlpatterns = [
-    path('register/', views.register_view, name='register_view' )
+    path('register/', views.register_view, name='register_view' ),
+    path('register/create/', views.register_create, name='register_create' ),
 ]
